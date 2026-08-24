d891aea49870fddb1874b1eda23b956d6481b2c4
GustavoKirschnick <gustavoborges107@gmail.com>
2026-08-24 16:31:02 -0300

    docs: add readme text

---
+++ src/pyplaneinertia/__init__.py
@@ -0,0 +1,13 @@
+from .models import AirfoilCoordinates, MassProperties, PositionVector, SectionGeometry
+from .section import Section
+from .surface import HorizontalSurface, VerticalSurface
+
+__all__ = [
+    "AirfoilCoordinates",
+    "MassProperties",
+    "PositionVector",
+    "SectionGeometry",
+    "Section",
+    "HorizontalSurface",
+    "VerticalSurface",
+]
